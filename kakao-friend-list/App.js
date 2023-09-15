849c9a127133b5db7da5b9b1863521869726a58b
pomiryu <daniel231963@naver.com>
2023-09-15 22:30:22 +0900

    04__safe-area-context 적용

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+import { SafeAreaProvider, SafeAreaView  } from 'react-native-safe-area-context';
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
 
-console.log(`${Platform.OS} :`, statusBarHeight, bottomSpace)
+console.log(`${Platform.OS} :`, statusBarHeight, bottomSpace) 
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-    </SafeAreaView>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView edges={['right', 'left']} style={styles.container}>
+        <Header />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -20,9 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: Platform.select({
-      android: statusBarHeight,
-    }),
+    paddingTop: statusBarHeight,
     // alignItems: 'center',
     justifyContent: 'flex-start',
   },
