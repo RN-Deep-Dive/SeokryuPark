db5b4b8cb99da2c000b29d2c869d58cc29cb0fab
pomiryu <daniel231963@naver.com>
2023-09-15 22:45:53 +0900

    04. UI개발(2) 내 프로필

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 import { SafeAreaProvider, SafeAreaView  } from 'react-native-safe-area-context';
+import MyProfile from './src/MyProfile';
+import { myProfile } from './src/data'
+import Margin from './src/Margin';
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
@@ -14,6 +17,14 @@ export default function App() {
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView edges={['right', 'left']} style={styles.container}>
         <Header />
+
+        <Margin height={10} />
+
+        <MyProfile 
+          name={myProfile.name}
+          uri={myProfile.uri}
+          intro={myProfile.introduction}
+          />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ kakao-friend-list/src/Margin.js
@@ -0,0 +1,8 @@
+import { View } from 'react-native'
+import React from 'react'
+
+const Margin = (props) => {
+  return <View style={{ height: props.height }}/>
+}
+
+export default Margin
